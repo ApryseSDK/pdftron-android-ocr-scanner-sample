bed74d8b4bcbab129b914406c39fae18af97672c
Branden Fung <bfung@pdftron.com>
2020-10-08 11:34:35 -0700

    Fix provider reference

--- client/scanlibrary/src/main/java/com/scanlibrary/PickImageFragment.java
+++ client/scanlibrary/src/main/java/com/scanlibrary/PickImageFragment.java
@@ -120,7 +120,7 @@ public class PickImageFragment extends Fragment {
         Log.d("", "openCamera: isDirectoryCreated: " + isDirectoryCreated);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             Uri tempFileUri = FileProvider.getUriForFile(getActivity().getApplicationContext(),
-                    "com.scanlibrary.provider", // As defined in Manifest
+                    getContext().getPackageName() + ".contentprovider", // As defined in Manifest
                     file);
             cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempFileUri);
         } else {
