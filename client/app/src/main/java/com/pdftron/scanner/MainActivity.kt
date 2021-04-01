dddfb8c3f81858c84f4a0aabf259fa4e18555e28
Branden Fung <bfung@pdftron.com>
2021-04-01 15:15:04 -0700

    Add support for Google MLKit

--- client/app/src/main/java/com/pdftron/scanner/MainActivity.kt
+++ client/app/src/main/java/com/pdftron/scanner/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -13,22 +14,20 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import com.google.firebase.FirebaseApp
-import com.google.firebase.storage.FirebaseStorage
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.pdftron.pdf.*
 import com.pdftron.pdf.config.ViewerConfig
 import com.pdftron.pdf.controls.DocumentActivity
+import com.pdftron.sdf.SDFDoc
 import com.scanlibrary.ScanConstants
 import com.scanlibrary.ScannerContract
 import com.scanlibrary.Utils
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
-import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,7 +38,7 @@ class MainActivity : AppCompatActivity() {
     private val bucket = "FIREBASE_STORAGE_BUCKET"
     private val cloudFunctionUrl: String = "CLOUD_FUNCTION_URL"
 
-    private val storage: FirebaseStorage = FirebaseStorage.getInstance(bucket)
+//    private val storage: FirebaseStorage = FirebaseStorage.getInstance(bucket)
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
@@ -50,6 +49,15 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    fun androidRectToPdfRect(androidRect: android.graphics.Rect, ratio: Double, imgHeight: Double): Rect {
+        return Rect(
+            androidRect.left.toDouble() * ratio,
+            (imgHeight - androidRect.bottom.toDouble()) * ratio,
+            androidRect.right.toDouble() * ratio,
+            (imgHeight - androidRect.top.toDouble()) * ratio
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,16 +72,114 @@ class MainActivity : AppCompatActivity() {
                 // Obtain the bitmap and save as a local image file
                 var bitmap: Bitmap? = null
                 bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+                val imgHeight = bitmap.height.toDouble()
+                val imgWidth = bitmap.width.toDouble()
                 contentResolver.delete(uri!!, null, null)
 
                 // Save bitmap to local cache as image then upload for processing
                 val localJpeg = Utils.saveBitmapAsJpeg(bitmap, filesDir)
 
                 // Process image on server
-                uploadFile(localJpeg)
+//                uploadFile(localJpeg)
+                val recognizer = TextRecognition.getClient()
+                val image = InputImage.fromFilePath(this, Uri.fromFile(localJpeg))
+                Log.d("ScannerSample", "Path is = " + localJpeg.absolutePath)
+                val result = recognizer.process(image)
+                    .addOnSuccessListener { visionText ->
+                        Log.d("ScannerSample", "Failed")
+                        val doc = PDFDoc()
+                        val outputPath = File(
+                            this.filesDir, com.pdftron.pdf.utils.Utils.getFileNameNotInUse(
+                                "scanned_doc.pdf"
+                            )
+                        )
+                        Convert.toPdf(doc, localJpeg.absolutePath)
+
+                        val page = doc.getPage(1)
+
+                        val ratioWidth = page.pageWidth / imgWidth;
+                        val ratioHeight = page.pageHeight / imgHeight;
+
+                        val resultText = visionText.text
+
+                        val jsonWords = JSONArray()
+
+
+                        for (block in visionText.textBlocks) {
+                            val blockText = block.text
+                            val blockCornerPoints = block.cornerPoints
+                            val blockFrame = block.boundingBox
+                            val pdfRect = androidRectToPdfRect(blockFrame!!, ratioWidth, imgHeight)
+
+//                            val sq1 = Square.create(doc, pdfRect)
+//                            sq1.setColor(ColorPt(0.0, 1.0, 0.0), 3)
+//                            sq1.refreshAppearance()
+//                            page.annotPushBack(sq1)
+
+                            val jsonLines = JSONArray()
+                            for (line in block.lines) {
+                                val lineText = line.text
+                                val lineCornerPoints = line.cornerPoints
+                                val lineFrame = line.boundingBox
+                                val pdfRect = androidRectToPdfRect(lineFrame!!, ratioWidth, imgHeight)
+
+//                                val sq2 = Square.create(doc, pdfRect)
+//                                sq2.setColor(ColorPt(0.0, 0.0, 1.0), 3)
+//                                sq2.refreshAppearance()
+//                                page.annotPushBack(sq2)
+
+                                for (element in line.elements) {
+                                    val elementText = element.text
+                                    val elementCornerPoints = element.cornerPoints
+                                    val elementFrame = element.boundingBox
+
+                                    val pdfRect = androidRectToPdfRect(elementFrame!!, ratioWidth, imgHeight)
+
+//                                    val sq3 = Square.create(doc, pdfRect)
+//                                    sq3.setColor(ColorPt(0.0, 0.0, 0.0), 3)
+//                                    sq3.refreshAppearance()
+//                                    page.annotPushBack(sq3)
+
+                                    val word = JSONObject()
+                                    word.put("font-size", (pdfRect.y2 - pdfRect.y1))
+                                    word.put("length", (pdfRect.x2 - pdfRect.x1))
+                                    word.put("text", elementText)
+                                    word.put("x", pdfRect.x1)
+                                    word.put("y", pdfRect.y1)
+                                    jsonWords.put(word)
+                                }
+                            }
+                        }
+
+                        val jsonObj = JSONObject()
+                        val jsonPages = JSONArray()
+
+                        val jsonPage = JSONObject()
+                        jsonPage.put("Word", jsonWords)
+                        jsonPage.put("num", 1) // Only supports one page
+                        jsonPage.put("dpi", 96)
+                        jsonPage.put("origin", "BottomLeft'")
+
+                        jsonPages.put(jsonPage)
+                        jsonObj.put("Page", jsonPages)
+
+                        OCRModule.applyOCRJsonToPDF(doc, jsonObj.toString());
+
+                        doc.save(outputPath.absolutePath, SDFDoc.SaveMode.INCREMENTAL, null)
+
+                        val config = ViewerConfig.Builder().openUrlCachePath(cacheDir.absolutePath).build()
+                        DocumentActivity.openDocument(
+                            this@MainActivity,
+                            Uri.fromFile(outputPath),
+                            config
+                        )
+                    }
+                    .addOnFailureListener { e ->
+                        Log.d("ScannerSample", "Failed")
+                    }
 
                 // Show progress UI
-                showProgress()
+//                showProgress()
             }
         }
 
@@ -97,69 +203,69 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun uploadFile(localFile: File) {
-        val reference = storage.reference
-        val fileName = localFile.name
-        val fileReference = reference.child(fileName)
-        val uploadTask = fileReference.putFile(Uri.fromFile(localFile))
-        // Register observers to listen for when the download is done or if it fails
-        uploadTask.addOnSuccessListener {
-            // If successful, we run our cloud function with the given file
-            runCloudFunction(fileName)
-        }
-    }
+//    private fun uploadFile(localFile: File) {
+//        val reference = storage.reference
+//        val fileName = localFile.name
+//        val fileReference = reference.child(fileName)
+//        val uploadTask = fileReference.putFile(Uri.fromFile(localFile))
+//        // Register observers to listen for when the download is done or if it fails
+//        uploadTask.addOnSuccessListener {
+//            // If successful, we run our cloud function with the given file
+//            runCloudFunction(fileName)
+//        }
+//    }
 
-    private fun runCloudFunction(fileName: String) {
-        // Call cloud function using HTTP request using OkHttp and RxJava
-        Single.create<String> {
-            // Create HTTP request to trigger cloud function
-            val httpBuilder = cloudFunctionUrl.toHttpUrlOrNull()!!.newBuilder()
-                .addQueryParameter("file", fileName)
-            val request = Request.Builder().url(httpBuilder.build()).build()
-            val client = OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS)
-                .writeTimeout(60, TimeUnit.SECONDS).callTimeout(60, TimeUnit.SECONDS).build()
-            val response = client.newCall(request).execute()
-            if (response.isSuccessful) {
-                it.onSuccess(response.body!!.string())
-            } else {
-                it.onError(IOException(response.message))
-            }
-        }.apply {
-            subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { it ->
-                    // If processing is successful, download processed file from Firebase Storage
-                    downloadStorageFile(it.replace("\"", "")) // trim result
+//    private fun runCloudFunction(fileName: String) {
+//        // Call cloud function using HTTP request using OkHttp and RxJava
+//        Single.create<String> {
+//            // Create HTTP request to trigger cloud function
+//            val httpBuilder = cloudFunctionUrl.toHttpUrlOrNull()!!.newBuilder()
+//                .addQueryParameter("file", fileName)
+//            val request = Request.Builder().url(httpBuilder.build()).build()
+//            val client = OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS)
+//                .writeTimeout(60, TimeUnit.SECONDS).callTimeout(60, TimeUnit.SECONDS).build()
+//            val response = client.newCall(request).execute()
+//            if (response.isSuccessful) {
+//                it.onSuccess(response.body!!.string())
+//            } else {
+//                it.onError(IOException(response.message))
+//            }
+//        }.apply {
+//            subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe { it ->
+//                    // If processing is successful, download processed file from Firebase Storage
+//                    downloadStorageFile(it.replace("\"", "")) // trim result
+//
+//                    // Optionally, delete uploaded file from Firebase Storage
+//                    deleteStorageFile(fileName)
+//                }
+//        }
+//    }
+//
+//    private fun downloadStorageFile(fileName: String) {
+//        val reference = storage.reference
+//        val fileReference = reference.child(fileName)
+//        val localFile = File(cacheDir, fileName)
+//
+//        fileReference.getFile(localFile).addOnSuccessListener {
+//            // Hide progress bar
+//            hideProgress()
+//
+//            // Open processed document in PDF viewer
+//            val config = ViewerConfig.Builder().openUrlCachePath(cacheDir.absolutePath).build()
+//            DocumentActivity.openDocument(this@MainActivity, Uri.fromFile(localFile), config)
+//
+//            // Optionally, delete processed file on Firebase Storage
+//            deleteStorageFile(fileName)
+//        }
+//    }
 
-                    // Optionally, delete uploaded file from Firebase Storage
-                    deleteStorageFile(fileName)
-                }
-        }
-    }
-
-    private fun downloadStorageFile(fileName: String) {
-        val reference = storage.reference
-        val fileReference = reference.child(fileName)
-        val localFile = File(cacheDir, fileName)
-
-        fileReference.getFile(localFile).addOnSuccessListener {
-            // Hide progress bar
-            hideProgress()
-
-            // Open processed document in PDF viewer
-            val config = ViewerConfig.Builder().openUrlCachePath(cacheDir.absolutePath).build()
-            DocumentActivity.openDocument(this@MainActivity, Uri.fromFile(localFile), config)
-
-            // Optionally, delete processed file on Firebase Storage
-            deleteStorageFile(fileName)
-        }
-    }
-
-    private fun deleteStorageFile(fileName: String) {
-        val reference = storage.reference
-        val fileReference = reference.child(fileName)
-        fileReference.delete()
-    }
+//    private fun deleteStorageFile(fileName: String) {
+//        val reference = storage.reference
+//        val fileReference = reference.child(fileName)
+//        fileReference.delete()
+//    }
 
     private fun showProgress() {
         progressBar.visibility = View.VISIBLE
